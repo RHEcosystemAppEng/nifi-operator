326b69a586e717fd2b75ff4387b8d3d62c2eb61b
Alejandro Villegas <alex.ansi.c@gmail.com>
2022-06-20 18:08:35 +0200

    First commit
    Signed-off-by: Alejandro Villegas <alex.ansi.c@gmail.com>

---
+++ api/v1alpha1/nifi_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NifiSpec defines the desired state of Nifi
+type NifiSpec struct {
+	//+kubebuilder:validation:Minimum=0
+	// Size is the size of the nifi deployment
+	Size int32 `json:"size"`
+}
+
+// NifiStatus defines the observed state of Nifi
+type NifiStatus struct {
+	// Nodes are the names of the nifi pods
+	Nodes []string `json:"nodes"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Nifi is the Schema for the nifis API
+type Nifi struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NifiSpec   `json:"spec,omitempty"`
+	Status NifiStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// NifiList contains a list of Nifi
+type NifiList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Nifi `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Nifi{}, &NifiList{})
+}
